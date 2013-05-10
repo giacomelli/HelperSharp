1e9a95cc73ad2cc8d5d71806585cd78bb3b334ee
Diego Giacomelli <giacomelli@gmail.com>
2013-05-10 15:30:48 -0300

    File charset test.

---
+++ HelperSharp.UnitTests/EnumerableHelperTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+
+namespace HelperSharp.UnitTests
+{
+	[TestFixture()]
+	public class EnumerableHelperTest
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
---
+++ HelperSharp/EnumerableHelper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelperSharp
+{
+	public class EnumerableHelper
+	{
+		public EnumerableHelper ()
+		{
+		}
+	}
+}
+
