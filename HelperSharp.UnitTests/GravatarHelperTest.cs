e9926230ada1e6d05fcdd295b2dd155311fd21a0
Diego Giacomelli <giacomelli@gmail.com>
2014-09-02 12:15:02 -0300

    Added the options below to GravatarHelper.GetAvatarUrl: * size * defaultImage * forceDefaultImage * rating * secureRequest

--- HelperSharp.UnitTests/GravatarHelperTest.cs
+++ HelperSharp.UnitTests/GravatarHelperTest.cs
@@ -25,6 +25,55 @@ namespace HelperSharp.UnitTests
 			expected = GravatarHelper.GetAvatarUrl (" GIACOMELLI@gmail.com ");
 			Assert.AreEqual ("http://www.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg", expected);
 		}
+
+        [Test()]
+        public void GetAvatarUrl_OnlyEmail_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com");
+            Assert.AreEqual("http://www.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg", expected);
+        }
+
+        [Test()]
+        public void GetAvatarUrl_EmailAndSize_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com", 32);
+            Assert.AreEqual("http://www.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg?s=32", expected);
+        }
+
+        [Test()]
+        public void GetAvatarUrl_EmailAndDefaultImage_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com", defaultImage: "blank");
+            Assert.AreEqual("http://www.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg?d=blank", expected);
+        }
+
+        [Test()]
+        public void GetAvatarUrl_EmailAndForceDefaultImage_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com", forceDefaultImage: true);
+            Assert.AreEqual("http://www.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg?f=y", expected);
+        }
+
+        [Test()]
+        public void GetAvatarUrl_EmailAndRating_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com", rating: "pg");
+            Assert.AreEqual("http://www.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg?r=pg", expected);
+        }
+
+        [Test()]
+        public void GetAvatarUrl_EmailAndSecureRequest_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com", secureRequest: true);
+            Assert.AreEqual("http://secure.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg", expected);
+        }
+
+        [Test()]
+        public void GetAvatarUrl_EmailAndAllOptions_Url()
+        {
+            var expected = GravatarHelper.GetAvatarUrl("giacomelli@gmail.com", 64, "mm", true, "x", true);
+            Assert.AreEqual("http://secure.gravatar.com/avatar/c5c70a93a2b605756df1af5da0dd413f.jpg?s=64&d=mm&f=y&r=x", expected);
+        }
     }
 }
 
--- HelperSharp/GravatarHelper.cs
+++ HelperSharp/GravatarHelper.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace HelperSharp
@@ -12,18 +14,62 @@ namespace HelperSharp
         /// Gets the avatar URL for the specified e-mail.
         /// Original source code from: https://gist.github.com/danesparza/973923.
         /// </summary>
+        /// <remarks>
+        /// Take a look on https://en.gravatar.com/site/implement/images/ to see the paremeters options
+        /// </remarks>
         /// <returns>The avatar URL.</returns>
         /// <param name="email">The e-mail.</param>
+        /// <param name="size">The image size. Default is 80px.</param>
+        /// <param name="defaultImage">The default image. See options on https://en.gravatar.com/site/implement/images/.</param>
+        /// <param name="forceDefaultImage">If for some reason you wanted to force the default image to always load.</param>
+        /// <param name="rating">Gravatar allows users to self-rate their images so that they can indicate if an image is appropriate for a certain audience. Default 'g'.</param>
+        /// <param name="secureRequest">If you're displaying Gravatars on a page that is being served over SSL (e.g. the page URL starts with HTTPS), then you'll want to serve your Gravatars via SSL as well, otherwise you'll get annoying security warnings in most browsers.</param>
         [SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase")]
         [SuppressMessage("Microsoft.Design", "CA1055:UriReturnValuesShouldNotBeStrings")]
-        public static string GetAvatarUrl(string email)
+        public static string GetAvatarUrl(string email, int size = 80, string defaultImage = null, bool forceDefaultImage = false, string rating = "g", bool secureRequest = false)
         {
             ExceptionHelper.ThrowIfNullOrEmpty("email", email);
+            ExceptionHelper.ThrowIfNullOrEmpty("rating", rating);
 
             var sanitizedEmail = email.Trim().ToLowerInvariant();
             var avatarHash = MD5Helper.Encrypt(sanitizedEmail);
 
-            return "http://www.gravatar.com/avatar/{0}.jpg".With(avatarHash);
+            var url = "http://{0}.gravatar.com/avatar/{1}.jpg".With(
+                secureRequest ? "secure" : "www",
+                avatarHash);
+
+            var parameters = new List<string>();
+
+            // Size.
+            if (size != 80)
+            {
+                parameters.Add("s={0}".With(size));
+            }
+
+            // Default image.
+            if (!String.IsNullOrEmpty(defaultImage))
+            {
+                parameters.Add("d={0}".With(defaultImage));
+            }
+
+            // Force default image.
+            if (forceDefaultImage)
+            {
+                parameters.Add("f=y");
+            }
+
+            // Rating.
+            if (!rating.Equals("g", StringComparison.OrdinalIgnoreCase))
+            {
+                parameters.Add("r={0}".With(rating));
+            }
+
+            if (parameters.Count > 0)
+            {
+                url = "{0}?{1}".With(url, String.Join("&", parameters.ToArray()));
+            }
+
+            return url;
         }
     }
 }
