1dd6d9dc108eeaa29598620c5c9efa5c26302cc1
Eduardo Pereira da Silva <eduardobursa@gmail.com>
2018-12-25 12:29:01 -0200

    adding package information

---
+++ HelperSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[module: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
